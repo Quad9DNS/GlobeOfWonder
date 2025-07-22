58dba129ae59ca8aba5052eae258f8e061bab05f
Ensar Sarajčić <dev@ensarsarajcic.com>
2025-07-22 10:36:27 +0200

    Reduce automatic rotation speed with higher zoom levels
    Related: #10

--- src/globe/layers/rotation.ts
+++ src/globe/layers/rotation.ts
@@ -1,17 +1,27 @@
 import ThreeGlobe from "three-globe";
 import { Settings } from "../../settings";
-import { GlobeLayerFrameUpdateHook } from "../layer";
+import { GlobeLayerAppStateHook, GlobeLayerFrameUpdateHook } from "../layer";
+import { AppState } from "../../service/state";
 
 /**
  * Globe layer that automatically rotates the globe (if the setting is active)
  * NOTE: this layer is frame rate dependent - higher frame rates will result in faster rotation,
  * but this produces a better looking effect when the frame rate is low
  */
-export class RotationLayer implements GlobeLayerFrameUpdateHook {
+export class RotationLayer
+  implements GlobeLayerAppStateHook, GlobeLayerFrameUpdateHook
+{
   readonly layerName: string = "Rotation";
+
+  private state!: AppState;
+
+  attachToState(state: AppState): void {
+    this.state = state;
+  }
+
   updateFrame(globe: ThreeGlobe, settings: Settings): void {
     if (settings.autoRotateGlobe) {
-      globe.rotation.y += 0.001;
+      globe.rotation.y += 0.001 / (10 * this.state.globeCurrentZoomFactor);
     }
   }
 }
