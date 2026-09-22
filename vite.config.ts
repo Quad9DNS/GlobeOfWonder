9b2beb73a00cc5a6efba51e1763fddecafea0957
Ensar Sarajčić <dev@ensarsarajcic.com>
2026-09-22 09:33:04 +0200

    Fix vite build for decorators

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, withFilter } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 import svgLoader from "vite-svg-loader";
+import swc from "@rollup/plugin-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [viteSingleFile(), svgLoader()],
+  plugins: [
+    viteSingleFile(),
+    svgLoader(),
+    withFilter(
+      swc({
+        swc: {
+          jsc: {
+            parser: { decorators: true, decoratorsBeforeExport: true },
+            transform: { decoratorVersion: "2023-11" },
+          },
+        },
+      }),
+      // Only run this transform if the file contains a decorator.
+      { transform: { code: "@", id: /\.ts/ } },
+    ),
+  ],
   esbuild: {
     target: "ES2020",
   },
